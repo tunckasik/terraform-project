719336219a31215d7ce89739bad2a5c51d97b423
tunckasik <tunckasikali@gmail.com>
2022-12-17 13:33:22 -0500

    fixed the variable region name  issue-

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "rg_name" {
   description = "The Azure region to create things in."
   default     = "tf-remote-rg"
 }
-variable "rg_region" {
+variable "region" {
   description = "The Azure region to create things in."
   default     = "East US"
 }
